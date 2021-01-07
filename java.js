c44cac66cc74e58816d2fa0c3d534b4a1b5415e3
SUNNATILLA <qwert@hd-mail.com>
2021-01-07 12:01:37 +0500

    java

---
+++ java.js
@@ -0,0 +1,44 @@
+
+
+alert (' Assalom alekum  xush kelibsiz ')
+
+var nick = prompt('Ismingiz nima')
+console.log( nick );
+alert("Salom " + nick)
+
+var nun  = prompt ('Yoshingiz nechida');
+console.log( " Mening yoshi  " + nun  + " da");
+alert(' O`qish uchun zor yosh ')
+
+
+
+
+
+
+var nun = prompt('Misol :' + ' 5 * 5 + 25 = ?')
+console.log(" Misol 1: " + 'To`g`ri javob ' + ( 5 * 5 + 25) + ' sizning jovobinggiz ' + nun);
+
+var nun = prompt('Misol :' + ' 5 * 5 = ?')
+console.log(" Misol 2: " + 'To`g`ri javob ' + ( 5 * 5) + ' sizning jovobinggiz ' + nun);
+
+var nun = prompt ( 'Misol :' + ' 9 / 3 = ?')
+console.log(" Misol 3: " + 'To`g`ri javob ' + (9 / 3 ) + ' sizning jovobinggiz ' + nun );
+
+var nun = prompt ( 'Misol :' + ' 15 + 5 = ?')
+console.log(" Misol 4: " + 'To`g`ri javob ' + (15 + 5) + ' sizning jovobinggiz ' + nun );
+
+var nun = prompt ( 'Misol :' + ' 20 - 10 = ?')
+console.log(" Misol 5: " + 'To`g`ri javob ' + (20 - 10) + ' sizning jovobinggiz ' + nun );
+
+var nun = prompt ( 'Misol :' + ' 10 / 3 = ?')
+console.log(" Misol 6: " + 'To`g`ri javob ' + (10 / 3) + ' sizning jovobinggiz ' + nun );
+
+var  con = alert ('Javoblarni ko`rish uchun consolni oching ') 
+
+
+var a =prompt("Mani qilgan ishimga nechi baho qo`yasiz")
+console.log("Sizning meniga qo`ygan bahogiz " + a + " 😱😱😱😱😱 "  + "raxmat" );
+
+var b = alert(' Etibor uchun  katta raxmat  😀😀😀😀 ')  
+
+        
